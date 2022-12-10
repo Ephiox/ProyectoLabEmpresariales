6c6fd032eca0d43ec1478f356ec8c3ff11414c04
ale <ephiox@gmail.com>
2022-12-10 13:27:03 +0100

    1º Implementacion de Base de datos

--- baseDatos.py
+++ baseDatos.py
@@ -1,11 +1,9 @@
 import sqlite3
 
-conn = sqlite3.connect('log.sqlite')
+conn = sqlite3.connect('bd.sqlite')
 cursor = conn.cursor()
 
-_SQL = """insert into log
-          (phrase, letters, ip, browser_string,results)
-          values
-          ('galaxia','aeiou','127.0.0.1','Chrome',"{'a','i'}")"""
-cursor.execute(_SQL)
-conn.commit()
+cursor.execute("""INSERT INTO search_log (name, dob)
+            VALUES (?, ?)""", (new_name, new_dob))
+
+
---
+++ crearbaseDatos.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('bd.sqlite')
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE search_log (
+                id int auto_increment primary key,
+                ts timestamp default current_timestamp,
+                phrase varchar(128) not null,
+                letters varchar(32) not null,
+                ip varchar(16) not null,
+                browser_string varchar(256) not null,
+                results varchar(64) not null );""")
+
+cursor.execute("""CREATE TABLE usuario (
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+                name TEXT NOT NULL,
+                dob DATE NOT NULL )""")
+conn.commit()
