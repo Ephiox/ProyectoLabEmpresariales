7ad9477d73e8a491621f3fc9d39e41f9aff0cda7
ale <ephiox@gmail.com>
2022-12-17 13:04:59 +0100

    1º Implementacion del Register, Login y Anonimous

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, request, redirect,escape
+from flask import Flask, render_template, request, redirect, escape
 from markupsafe import escape
-from search4web import search4letters, log_request, readlog_db
+from search4web import search4letters, log_request
+from baseDatos import log_request_bd, readlog_bd, register_bd, login_bd
 
 app = Flask(__name__)
+
 
 @app.route("/search", methods=['POST'])
 def search():
     return str(search4letters(request.form['phrase'], request.form['letters']))
 
 
-@app.route("/")
 @app.route("/entry")
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search for letters on the web!')
+
 
 @app.route("/")
 @app.route("/index")
@@ -21,12 +23,20 @@ def index_page() -> 'html':
     return render_template('index.html',
                            the_title='Welcome to search for letters on the web!')
 
-@app.route("/")
-@app.route("/loggin")
-def index_page() -> 'html':
-    checkear
-    return render_template('index.html',
-                           the_title='Welcome to search for letters on the web!')
+
+@app.route("/login", methods=['POST'])
+def login_page() -> 'html':
+    action = request.form['action']
+    if action == 'Register':
+        register_bd(request)
+        return render_template('login.html',
+                               the_title=action)
+    elif action == 'Login':
+        login_bd(request)
+        return redirect("/entry", code=302)
+    elif action == 'Anonymous':
+        return redirect("/entry", code=302)
+
 
 @app.route("/result", methods=['POST'])
 def results_page():
@@ -34,14 +44,15 @@ def results_page():
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
-    log_request(request,results)
+    log_request_bd(request, results)
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
                            the_letters=letters,
                            the_results=results)
 
+
 @app.route('/viewlog')
 def view_the_log() -> str:
-    contents = readlog_db()
+    contents = readlog_bd()
     return escape(contents)
--- baseDatos.py
+++ baseDatos.py
@@ -1,8 +1,76 @@
 import sqlite3
+from datetime import datetime
+import string
+import time
+import random
 
-conn = sqlite3.connect('bd.sqlite')
-cursor = conn.cursor()
 
-cursor.execute("""SELECT * FROM search_log""")
-res = cursor.fetchall()
-print(res)
+def check_bd():
+    conn = sqlite3.connect('bd.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT * FROM users""")
+    res = cursor.fetchall()
+    return res
+
+
+def log_request_bd(req: 'flask_request', res: str) -> None:
+    conn = sqlite3.connect('bd.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO search_log (phrase, letters, ip, browser_string, results)
+                VALUES (?, ?, ?, ?, ?)""", (req.form['phrase'], req.form['letters'],
+                                            req.remote_addr, str(req.user_agent),
+                                            res))
+    conn.commit()
+
+
+def readlog_bd():
+    conn = sqlite3.connect('bd.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM search_log""")
+    res = cursor.fetchall()
+    return res
+
+
+def register_bd(req: 'flask_request'):
+    conn = sqlite3.connect('bd.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO users (name, password, dob)
+                VALUES (?, ?, ?)""", (req.form['usuario'], req.form['password'], str(datetime.today())))
+    conn.commit()
+
+
+def login_bd(req: 'flask_request'):
+    conn = sqlite3.connect('bd.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT password FROM users WHERE name=(?)""", (req.form['usuario'],))
+    res = cursor.fetchall()
+    token = ''
+    if not res:
+        token = 'Error'
+    elif res[0][0] == req.form['password']:
+        token = token_generator(64)
+        cursor.execute("""UPDATE users set temporalID = (?) WHERE name= (?)""", (token, req.form['usuario']))
+        conn.commit()
+    return token
+
+
+def token_generator(longitud):
+    alphabet = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+    specials = [',', '.', '*', '?', '%', '$', '!']
+    numbers = list(range(0, 100))
+
+    # Se ha sumado varias veces el alfabeto y los caracteres especiales
+    # para aumentar la probabilidad de estos
+    alphabet = alphabet + alphabet + specials + specials + specials + numbers
+
+    semilla = round(time.time())
+    random.seed(semilla)
+
+    password = []
+    for i in range(0, longitud):
+        index = random.randint(0, len(alphabet) - 1)
+        password.append(str(alphabet[index]))
+    password = ''.join(password)
+    return password
