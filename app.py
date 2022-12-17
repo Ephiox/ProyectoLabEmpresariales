add65de62c6d53fe61376119c4fa28e8a82bbbea
ale <ephiox@gmail.com>
2022-12-17 16:10:45 +0100

    Navegavilidad

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, redirect, escape
+from flask import Flask, render_template, request, redirect, escape, session
+from flask_session import Session
 from markupsafe import escape
 from search4web import search4letters, log_request
-from baseDatos import log_request_bd, readlog_bd, register_bd, login_bd
+from baseDatos import log_request_bd, readlog_bd, register_bd, login_bd, check_token
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route("/search", methods=['POST'])
@@ -32,10 +36,26 @@ def login_page() -> 'html':
         return render_template('login.html',
                                the_title=action)
     elif action == 'Login':
-        login_bd(request)
-        return redirect("/entry", code=302)
+        token = login_bd(request)
+        if token != 'Error':
+            session["name"] = request.form['usuario']
+            #session['token'] = token
+            return redirect("/entry", code=302)
+        else:
+            return redirect("/", code=302)
     elif action == 'Anonymous':
         return redirect("/entry", code=302)
+
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+
+
+@app.route("/stats")
+def stats():
+    return render_template('stats.html')
 
 
 @app.route("/result", methods=['POST'])
@@ -44,7 +64,8 @@ def results_page():
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
-    log_request_bd(request, results)
+    #check = check_token(session.get("token"), request)
+    log_request_bd(session.get('name'), request, results)
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
